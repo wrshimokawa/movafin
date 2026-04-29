149a1b6a18d939f99ad54cf00a820b2e8f94aad4
Walderson Ryuiti Shimokawa <walderson.ryuiti@sefaz.mt.gov.br>
2026-04-29 21:43:42 +0000

    Pode ajustar o arquivo ;idx/dev.nix para subir os emuladores quando o am

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,41 +1,28 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
-{pkgs}: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+{ pkgs, ... }: {
+  channel = "stable-24.05";
   packages = [
-    pkgs.nodejs_22
-    pkgs.zulu
+    pkgs.nodejs_20
+    pkgs.firebase-tools
   ];
-  # Sets environment variables in the workspace
-  env = {};
-  # This adds a file watcher to startup the firebase emulators. The emulators will only start if
-  # a firebase.json file is written into the user's directory
-  services.firebase.emulators = {
-    # Disabling because we are using prod backends right now
-    detect = false;
-    projectId = "demo-app";
-    services = ["auth" "firestore"];
-  };
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "sswg.beam"
     ];
     workspace = {
       onCreate = {
-        default.openFiles = [
-          "src/app/page.tsx"
-        ];
+        npm-install = "npm install";
+      };
+      onStart = {
+        # Inicia os emuladores em background quando o ambiente inicia
+        # Note: O comando usa --import para persistir dados se necessário futuramente
+        firebase-emulators = "firebase emulators:start --only auth,firestore";
       };
     };
-    # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          command = [ "npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0" ];
           manager = "web";
         };
       };
