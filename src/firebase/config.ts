2ca49e2296963b65d8ab6a21f19e0501d6361d24
Walderson Ryuiti Shimokawa <walderson.ryuiti@sefaz.mt.gov.br>
2026-04-29 21:32:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2311387901-7dbbd",
+  "appId": "1:1074633629952:web:3c5082ca7039cc509f13e5",
+  "apiKey": "AIzaSyD4sne8Mi2bhqYl0imPZdvWBakcRaHDxEA",
+  "authDomain": "studio-2311387901-7dbbd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1074633629952"
+};
